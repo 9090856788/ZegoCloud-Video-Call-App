27ab52155f8e9ad1aa6b0850652e9391ffa4da6c
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-06-08 20:53:54 +0530

    added hero image & configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Room from "./components/Room";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/room/:id" element={<Room />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      <h1>Connect With Your Loved Ones Via ZeroCloud</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
